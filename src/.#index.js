2c458dbc3da92d78990d173373e60812076996f6
Rick Nunes <sanunes.ricardo@gmail.com>
2019-03-18 20:03:51 -0400

    Started reading list project

---
+++ src/.#index.js
@@ -0,0 +1,1 @@
+rick@Ricardos-MBP-2.68282
--- src/App.js
+++ src/App.js
@@ -1,28 +1,119 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+	<div className="container">
+	   <div className="row">
+	     <div className="col-lg p-down  bg-dark text-white-50">
+               <h1>Books I am Reading</h1>
+             </div>
+	   </div>
+	</div>
+
+	<div class="container">
+	  <div class="row">
+	    <div class="col-4 bg-primary text-white">TITLE</div>
+	    <div class="col-4 bg-secondary text-white-50">PROGRESS</div>
+	    <div class="col-4 bg-dark text-white">AUTHOR</div>
+          </div>
+          <Books />
+        </div>
       </div>
     );
   }
 }
+
+class Book extends React.Component {
+    percentageBook(page, total){
+	var percentage = (page/total)*100;
+	return percentage.toFixed(1);
+    }
+
+    render() {
+	return (
+	<div class="row book">
+		<div class="col-4 bg-title">
+          <div class="name">
+            <p>{this.props.title}</p>
+          </div>
+        </div>
+		<div class="col-4 bg-progress">
+		{this.percentageBook(this.props.currentPage, this.props.totalPages)}%
+		<div class="progress">
+            <div
+              class="progress-bar"
+              role="progressbar"
+            style={{ width: this.percentageBook(this.props.currentPage, this.props.totalPages) + "%" }}
+              aria-valuenow={this.props.progress}
+              aria-valuemin="0"
+              aria-valuemax="100"
+            />
+          </div>
+        </div>
+        <div class="col-4 bg-author">
+          <div class="name">
+            <p>{this.props.author}</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+const BooksTest = {
+    1: {
+	title: "COMMON LISP: A Gentle Introduction to Symbolic Computation",
+	progress: 5,
+	author: "David S. Touretzky",
+	currentPage: 31,
+	totalPages: 587,
+    },
+    2: {
+	title: "The Joy of Clojure",
+	progress: 20,
+	currentPage: 83,
+	totalPages: 311,
+	author: "Michael Fogus, Chris Houser"
+    },
+    3: {
+	title: "Discipline and Punish",
+	progress: 80,
+	currentPage: 203,
+	totalPages: 333,
+	author: "Michael Foucalt"
+    },
+    4: {
+	title: "Go Programming Blueprints",
+	progress: 20,
+	currentPage: 139,
+        totalPages: 385,
+	author: "Mat Ryer"
+    }
+};
+
+class Books extends React.Component {
+  _renderObject() {
+    return Object.entries(BooksTest).map(([key, value], i) => {
+      return (
+        <Book
+          key={key}
+          title={value.title}
+          progress={value.progress}
+	  currentPage={value.currentPage}
+	  totalPages={value.totalPages}
+	  author={value.author}
+        />
+      );
+    });
+  }
+  render() {
+    return <div>{this._renderObject()}</div>;
+  }
+}
+
 
 export default App;
